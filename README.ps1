e7fc967cef6675b44b7eae2bc330b55f7d726928
Brandon Olin <bolin@columbia.com>
2016-07-26 09:48:20 -0700

    Add readme

---
+++ README.ps1
@@ -0,0 +1,7 @@
+[![build status](http://gitlab.columbia.csc/SSE/OVF.Windows.Server/badges/master/build.svg)](http://gitlab.columbia.csc/SSE/OVF.Windows.Server/commits/master)
+
+# OVF.Windows.Server
+
+## Overview
+This is a set of Pester / poshspec tests designed to test the operation of Windows server.
+
