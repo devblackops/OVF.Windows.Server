389609549bc1d6cbba52a478b27d4c68a1db3e85
Brandon Olin <brandon@devblackops.io>
2016-08-14 15:51:46 -0700

    whitespace

--- OVF.Windows.Server/Diagnostics/Simple/Network.tests.ps1
+++ OVF.Windows.Server/Diagnostics/Simple/Network.tests.ps1
@@ -7,10 +7,10 @@ param(
 Import-Module -Name poshspec -Verbose:$false -ErrorAction Stop
 
 describe 'Network Adapters' {
-    context 'Availability' {        
+    context 'Availability' {
         $adapters = Get-NetAdapter -Name $Interfaces -Physical
         $adapters | % {
-            interface $_.Name Status { should be up }            
+            interface $_.Name Status { should be up }
         }        
     }
 }
