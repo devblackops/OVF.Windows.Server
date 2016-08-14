adcb3c93c7d1265d9c8ea929f8b8d93110b5caae
Brandon Olin <brandon@devblackops.io>
2016-08-14 13:45:45 -0700

    Rename PSDeploy file and set to PSGallery

--- ovf.psdeploy.ps1
+++
@@ -1,9 +0,0 @@
-Deploy OVF-Module {        
-    By PSGalleryModule Artifactory {
-        FromSource '.\OVF.Windows.Server\'
-        To 'Artifactory'
-        WithOptions @{
-            ApiKey = $env:ARTIFACTORY_API_KEY
-        }
-    }       
-}
